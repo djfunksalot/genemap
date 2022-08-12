5183f2b8ed0e6af6156e62456595cdf28fbdefb0
Steven Vitale <steve@newsie.net>
2022-08-12 08:31:25 -0400

    resize images, change name

--- src/App.js
+++ src/App.js
@@ -1,31 +1,32 @@
 import React,{useState} from 'react'
 import './App.css';
 import JSONDATA from './genes.json'
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link,
-  useParams
-} from "react-router-dom";
 function App() {
   const [searchTerm,setSearchTerm] = useState('')
   return (
     <div className="App">
       <input type="text" placeholder="seach..." onChange={e=>setSearchTerm(e.target.value)} />
       {JSONDATA.filter((val)=>{
-        if(searchTerm == ""){
+        if(searchTerm === ""){
           return false
         }
-        else if(val.name.toLowerCase() == searchTerm.toLowerCase()){
+        else if(val.name.toLowerCase() === searchTerm.toLowerCase()){
           return val;
         }
       }).map((val,key)=>{
 
         return <div key={val.name}>
                          <div>{val.name}</div>
-                         <img src={process.env.PUBLIC_URL + "/images/"+ val.images.featureplot+".png"} />
-                         <img src={process.env.PUBLIC_URL + "/images/"+ val.images.dotplot+".png"} />
+                         <img 
+                             src={process.env.PUBLIC_URL + "/images/"+ val.images.featureplot+".jpeg"} 
+                             style={{ width: "100%", height: "100%" }} 
+                             alt="Feature Plot"
+                         />
+                         <img 
+                             src={process.env.PUBLIC_URL + "/images/"+ val.images.dotplot+".jpeg"} 
+                             style={{ width: "100%", height: "100%" }} 
+                             alt="Dotplot"
+                         />
 	</div>
       })}
     </div>
