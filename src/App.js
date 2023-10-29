782aaf2a87406711e6dbbe0270c485fe4f694a80
Steven Vitale <steve@newsie.net>
2023-10-29 17:56:22 -0400

    Get ready for updates

--- src/App.js
+++ src/App.js
@@ -1,43 +1,28 @@
-import React,{useState} from 'react'
+import React from 'react'
+import { BrowserRouter,NavLink, Route, Routes } from 'react-router-dom';
 import './App.css';
-import JSONDATA from './genes.json'
+import ScRNA from './components/scRNA/scRNA';
+import SnATAC from './components/snATAC/snATAC';
+import SnRNA from './components/snRNA/snRNA';
+import SpRNA from './components/spRNA/spRNA';
+const Home = () => <h1>Home</h1>;
 function App() {
-  const [searchTerm,setSearchTerm] = useState('')
   return (
-    <div className="App">
-      <input type="text" placeholder="seach..." onChange={e=>setSearchTerm(e.target.value)} />
-      {JSONDATA.filter((val)=>{
-        if(searchTerm === ""){
-          return false
-        }
-        else if(val.name.toLowerCase() === searchTerm.toLowerCase()){
-          return val;
-        }
-      }).map((val,key)=>{
-
-        return <div key={val.name}>
-                         <div>
-                         <img 
-                             src={process.env.PUBLIC_URL + "/images/umap.jpeg"} 
-                             style={{ width: "30%", height: "100%" }} 
-                             alt="Dotplot"
-                         />
-                         <img 
-                             src={process.env.PUBLIC_URL + "/images/"+ val.images.dotplot+".jpeg"} 
-                             style={{ width: "50%", height: "100%" }} 
-                             alt="Dotplot"
-                         />
-                         </div>
-                         <div>
-                         <img 
-                             src={process.env.PUBLIC_URL + "/images/"+ val.images.featureplot+".jpeg"} 
-                             style={{ width: "100%", height: "100%" }} 
-                             alt="Feature Plot"
-                         />
-                         </div>
-	</div>
-      })}
-    </div>
+    <BrowserRouter  basename={process.env.PUBLIC_URL}>
+<div>
+      <NavLink  className={({ isActive }) => (isActive ? 'active' : 'inactive')} to='/scRNA'>scRNA</NavLink>{' '}
+      <NavLink  className={({ isActive }) => (isActive ? 'active' : 'inactive')} to='/snATAC'>snATAC</NavLink>{' '}
+      <NavLink  className={({ isActive }) => (isActive ? 'active' : 'inactive')} to='/snRNA'>snRNA</NavLink>{' '}
+      <NavLink  className={({ isActive }) => (isActive ? 'active' : 'inactive')} to='/spRNA'>spRNA</NavLink>{' '}
+      <Routes>
+        <Route path="/scRNA" element={<ScRNA/>} />
+        <Route path="/snATAC" element={<SnATAC/>} />
+        <Route path="/snRNA" element={<SnRNA/>} />
+        <Route path="/spRNA" element={<SpRNA/>} />
+        <Route render={() => <h1>Page not found</h1>} />
+      </Routes>
+</div>
+</BrowserRouter>
   );
 }
 export default App;
