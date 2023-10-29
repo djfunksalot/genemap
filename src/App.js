5c8fa6a2dfeaa87d7bf99ee58914fb73b8682831
Steven Vitale <steve@newsie.net>
2023-10-29 18:05:40 -0400

    update spRNA link

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import './App.css';
 import ScRNA from './components/scRNA/scRNA';
 import SnATAC from './components/snATAC/snATAC';
 import SnRNA from './components/snRNA/snRNA';
-import SpRNA from './components/spRNA/spRNA';
 const Home = () => <h1>Home</h1>;
 function App() {
   return (
@@ -13,12 +12,11 @@ function App() {
       <NavLink  className={({ isActive }) => (isActive ? 'active' : 'inactive')} to='/scRNA'>scRNA</NavLink>{' '}
       <NavLink  className={({ isActive }) => (isActive ? 'active' : 'inactive')} to='/snATAC'>snATAC</NavLink>{' '}
       <NavLink  className={({ isActive }) => (isActive ? 'active' : 'inactive')} to='/snRNA'>snRNA</NavLink>{' '}
-      <NavLink  className={({ isActive }) => (isActive ? 'active' : 'inactive')} to='/spRNA'>spRNA</NavLink>{' '}
+      <a href="/samui/from.html?url=susztaklab.com/samui/files/&s=sample_HK2671&s=sample_HK2770&s=sample_HK2844&s=sample_HK2852&s=sample_HK2871&s=sample_HK2873&s=sample_HK3035">spRNA</a>
       <Routes>
         <Route path="/scRNA" element={<ScRNA/>} />
         <Route path="/snATAC" element={<SnATAC/>} />
         <Route path="/snRNA" element={<SnRNA/>} />
-        <Route path="/spRNA" element={<SpRNA/>} />
         <Route render={() => <h1>Page not found</h1>} />
       </Routes>
 </div>
