a9e99d709b396cab79fc827ecd56315732baf6fc
Steven Vitale <steve@newsie.net>
2023-11-08 07:18:58 -0500

    modify for txt tables

--- src/App.js
+++ src/App.js
@@ -1,43 +1,22 @@
-import React,{useState} from 'react'
+import React from 'react'
+import { BrowserRouter,NavLink, Route, Routes } from 'react-router-dom';
 import './App.css';
-import JSONDATA from './genes.json'
+import RNA from './components/RNA/';
+import ATAC from './components/ATAC/';
+const Home = () => <h1>Home</h1>;
 function App() {
-  const [searchTerm,setSearchTerm] = useState('')
   return (
-    <div className="App">
-      <input type="text" placeholder="seach..." onChange={e=>setSearchTerm(e.target.value)} />
-      {JSONDATA.filter((val)=>{
-        if(searchTerm === ""){
-          return false
-        }
-        else if(val.name.toLowerCase() === searchTerm.toLowerCase()){
-          return val;
-        }
-      }).map((val,key)=>{
-
-        return <div key={val.name}>
-                         <div>
-                         <img 
-                             src={process.env.PUBLIC_URL + "/images/umap.jpeg"} 
-                             style={{ width: "30%", height: "100%" }} 
-                             alt="Dotplot"
-                         />
-                         <img 
-                             src={process.env.PUBLIC_URL + "/images/"+ val.images.dotplot+".jpeg"} 
-                             style={{ width: "50%", height: "100%" }} 
-                             alt="Dotplot"
-                         />
-                         </div>
-                         <div>
-                         <img 
-                             src={process.env.PUBLIC_URL + "/images/"+ val.images.featureplot+".jpeg"} 
-                             style={{ width: "100%", height: "100%" }} 
-                             alt="Feature Plot"
-                         />
-                         </div>
-	</div>
-      })}
-    </div>
+    <BrowserRouter  basename={process.env.PUBLIC_URL}>
+<div>
+      <NavLink  className={({ isActive }) => (isActive ? 'active' : 'inactive')} to='/RNA'>RNA</NavLink>{' '}
+      <NavLink  className={({ isActive }) => (isActive ? 'active' : 'inactive')} to='/ATAC'>ATAC</NavLink>{' '}
+      <Routes>
+        <Route path="/RNA" element={<RNA/>} />
+        <Route path="/ATAC" element={<ATAC/>} />
+        <Route render={() => <h1>Page not found</h1>} />
+      </Routes>
+</div>
+</BrowserRouter>
   );
 }
 export default App;
