720027f5adc572f87502a36299c937f83f116d6d
Steven Vitale <steve@newsie.net>
2023-11-12 09:49:58 -0500

    working on filter

--- src/components/RNA/index.js
+++ src/components/RNA/index.js
@@ -1,50 +1,64 @@
 import React,{useState,useEffect} from 'react'
 import JSONDATA from './genes.json';
 export default function RNA() {
-    const [geneName, setGeneName] = React.useState('');
-    const [data, setData] = React.useState(null);
-	  const [loading, setLoading] = useState(true);
+  const [geneName, setGeneName] = React.useState('');
+  const [data, setData] = React.useState(null);
+  const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
-  const fetchData = async (uuid) => {
+  const [uuid,setUuid] = useState('');
+
+	const fetchData = async (uuid) => {
+		   uuid.preventDefault();
+    console.log("refresh prevented");
     try {
-                let response = await fetch("https://s3.us-east-2.amazonaws.com/ksusztak.genemap/JL/"+ uuid+".json", {mode:'no-cors'});
+      let id = 'ffeed4d2-7d6f-4dc1-a295-e8f407ced508';
+   let response =    fetch("https://s3.us-east-2.amazonaws.com/ksusztak.genemap/JL/"+ id+".json", {mode:'no-cors'})
       let result = await response.json();
 	    console.log("result")
-	    console.log(result)
-      setData(result);
+	    console.log("response")
+      setData("something");
       setLoading(false);
     } catch (err) {
+	    console.log("error")
+	    console.log(err)
       setError(err.message);
       setLoading(false);
     }
+//		   },[])
+
   }
 
 
 
-
   return (
+	  <div>
     <div className="wrapper">
-      <input type="text" placeholder="seach..." onChange={e=>setGeneName(e.target.value)} />
+      <input type="text" placeholder="seach..." onChange={e=>fetchData(e.target.value)} />
       {JSONDATA.filter((val)=>{
         if(geneName === ""){
           return false
         }
         else if(val.name.toLowerCase() === geneName.toLowerCase()){
-		fetchData(val.uuid)
-
-		console.log(val)
           return val;
         }
       }).map((val,key)=>{
-
-
-
+	      //{setLoading(true)}
+	     // {setUuid(val.uuid)}
+//	{FetchData(val.uuid)} 
         return <div key={val.name}>
-		      <div>{JSON.stringify(data)}</div>;
-                         <div>
-                         </div>
+		      {val.uuid}
+
+	         <form onSubmit={fetchData}>
+		              <input type="submit" value="Submit" />
+      </form>
+
+
+
 	</div>
       })}
     </div>
+    <div className="wrapper">
+    </div>
+	  </div>
   );
 }
