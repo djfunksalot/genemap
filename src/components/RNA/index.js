2d1b44b2dbe1d47bb6dfaae17fec38aa4931cb50
Steven Vitale <steve@newsie.net>
2023-11-12 11:01:39 -0500

    working on genes

--- src/components/RNA/index.js
+++ src/components/RNA/index.js
@@ -1,64 +1,87 @@
-import React,{useState,useEffect} from 'react'
+import React, {
+    useState,
+    useEffect
+} from 'react'
+
 import JSONDATA from './genes.json';
+import {
+  createColumnHelper,
+  flexRender,
+  getCoreRowModel,
+  useReactTable,
+} from '@tanstack/react-table'
 export default function RNA() {
-  const [geneName, setGeneName] = React.useState('');
-  const [data, setData] = React.useState(null);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(null);
-  const [uuid,setUuid] = useState('');
+    const [data, setData] = React.useState(null);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
+    const [uuid, setUuid] = useState('');
 
-	const fetchData = async (uuid) => {
-		   uuid.preventDefault();
-    console.log("refresh prevented");
-    try {
-      let id = 'ffeed4d2-7d6f-4dc1-a295-e8f407ced508';
-   let response =    fetch("https://s3.us-east-2.amazonaws.com/ksusztak.genemap/JL/"+ id+".json", {mode:'no-cors'})
-      let result = await response.json();
-	    console.log("result")
-	    console.log("response")
-      setData("something");
-      setLoading(false);
-    } catch (err) {
-	    console.log("error")
-	    console.log(err)
-      setError(err.message);
-      setLoading(false);
+const defaultData: Person[] = [
+  {
+    firstName: 'tanner',
+    lastName: 'linsley',
+    age: 24,
+    visits: 100,
+    status: 'In Relationship',
+    progress: 50,
+  },
+  {
+    firstName: 'tandy',
+    lastName: 'miller',
+    age: 40,
+    visits: 40,
+    status: 'Single',
+    progress: 80,
+  },
+  {
+    firstName: 'joe',
+    lastName: 'dirte',
+    age: 45,
+    visits: 20,
+    status: 'Complicated',
+    progress: 10,
+  },
+]
+
+
+    const fetchGene = (name) => {
+        {
+            JSONDATA.filter((val) => {
+                if (val.name.toLowerCase() === name.toLowerCase()) {
+                    setUuid(val.uuid)
+                         fetch("https://s3.us-east-2.amazonaws.com/ksusztak.genemap/JL/"+ val.uuid +".json")
+    .then(response => response.json())
+    .then(data => setData(data))
+                } else {
+			setData(null)
+		}
+            })
+        }
     }
-//		   },[])
-
-  }
 
 
 
-  return (
-	  <div>
-    <div className="wrapper">
-      <input type="text" placeholder="seach..." onChange={e=>setGeneName(e.target.value)} />
-      {JSONDATA.filter((val)=>{
-        if(geneName === ""){
-          return false
+    return ( <div>
+        <div className = "wrapper" >
+        <input type = "text" placeholder = "seach..." 
+        onChange = {
+            e => fetchGene(e.target.value)
         }
-        else if(val.name.toLowerCase() === geneName.toLowerCase()){
-          return val;
-        }
-      }).map((val,key)=>{
-	      //{setLoading(true)}
-	     // {setUuid(val.uuid)}
-//	{FetchData(val.uuid)} 
-        return <div key={val.name}>
-		      {val.uuid}
-
-	         <form onSubmit={fetchData}>
-		              <input type="submit" value="Submit" />
-      </form>
+        />
+        <div> {uuid} < /div> 
 
 
+      {data ? (
+        <div> {data.gene} < /div> 
 
-	</div>
-      })}
-    </div>
-    <div className="wrapper">
-    </div>
-	  </div>
-  );
+      ) : (
+        <div></div>
+      )}
+
+
+        </div>
+        <div className = "wrapper" >
+        </div> 
+        </div>
+    );
 }
