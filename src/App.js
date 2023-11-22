ff38d83bde4a452c47f1af1dc8c1b9630a0a911e
Steven Vitale <steve@newsie.net>
2023-11-21 23:02:32 -0500

    cleanup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import React from 'react'
 import { BrowserRouter,NavLink, Route, Routes } from 'react-router-dom';
 import './App.css';
-import RNA from './components/RNA/';
+import ScRNA from './components/scRNA/scRNA';
+import SnRNA from './components/snRNA/snRNA';
 const Home = () => <h1>Home</h1>;
 function App() {
-  return (RNA());
+  return (
+    <BrowserRouter  basename={process.env.PUBLIC_URL}>
+<div>
+      <NavLink  className={({ isActive }) => (isActive ? 'active' : 'inactive')} to='/scRNA'>scRNA</NavLink>{' '}
+      <NavLink  className={({ isActive }) => (isActive ? 'active' : 'inactive')} to='/snRNA'>snRNA</NavLink>{' '}
+      <Routes>
+        <Route path="/scRNA" element={<ScRNA/>} />
+        <Route path="/snRNA" element={<SnRNA/>} />
+        <Route render={() => <h1>Page not found</h1>} />
+      </Routes>
+</div>
+</BrowserRouter>
+  );
 }
 export default App;
